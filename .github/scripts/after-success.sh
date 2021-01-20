a13406d7af58c9096f965807f41032f072398fb5
sdarwin <samuel.d.darwin@gmail.com>
2021-01-20 19:27:47 +0000

    add drone config

--- .github/scripts/after-success.sh
+++ .github/scripts/after-success.sh
@@ -4,7 +4,7 @@
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
 
-if [ "$DRONE_JOB_UUID" = "b6589fc6ab" ] ; then
+if [ "$JOB_UUID" = "b6589fc6ab" ] ; then
     if [[ "$COVERALL" -ne "1" ]]; then exit 0; fi
     wget https://github.com/linux-test-project/lcov/archive/v1.13.zip
     unzip v1.13.zip
